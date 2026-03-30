80b6af387bc12631e896642626256d7033f7accb
Newstorerelogio <contato@newstorerj.com.br>
2026-03-30 15:04:23 -0300

    Update rastrear.js

--- pages/api/rastrear.js
+++ pages/api/rastrear.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
         "17token": API_KEY,
       },
-      body: JSON.stringify([{ number: cod }]),
+      body: JSON.stringify([{ number: cod, carrier: 3031 }]),
       signal: AbortSignal.timeout(10000),
     });
 
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
         "17token": API_KEY,
       },
-      body: JSON.stringify([{ number: cod }]),
+      body: JSON.stringify([{ number: cod, carrier: 3031 }]),
       signal: AbortSignal.timeout(10000),
     });
 
