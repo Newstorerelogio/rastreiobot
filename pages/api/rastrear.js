9a660335d70a95f6f7bae3a2acc6a9cfa085d4ce
Newstorerelogio <contato@newstorerj.com.br>
2026-03-30 15:15:32 -0300

    Update rastrear.js

--- pages/api/rastrear.js
+++ pages/api/rastrear.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     });
 
     // Passo 2: Buscar informações do rastreio
-    const res2 = await fetch("https://api.17track.net/track/v2.2/getRealTimeTrackInfo", {
+    const res2 = await fetch("https://api.17track.net/track/v2.2/gettrackinfo", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
